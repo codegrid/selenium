5618a1c134450a868b9b1cf74ab703db2eaee6d2
Kazuhito Hokamura <k.hokamura@gmail.com>
2014-11-20 17:06:35 +0900

    ログインエラーのテスト追加

--- tests/test/login.js
+++ tests/test/login.js
@@ -1,6 +1,7 @@
 var webdriver = require('selenium-webdriver');
 var firefox = require('selenium-webdriver/firefox');
 var t = require('selenium-webdriver/testing');
+var By = webdriver.By;
 var expect = require('expect.js');
 var driver;
 
@@ -21,6 +22,21 @@ t.describe('ログインページ', function() {
     driver.getCurrentUrl().then(function(url) {
       // ログインページにリダイレクトされているかを検証する
       expect(url).to.be('http://localhost:4000/login');
+    });
+  });
+
+  t.it('username/passwordが間違っていた場合にエラーが表示される', function() {
+    // ログインページにアクセス
+    driver.get('http://localhost:4000/login');
+
+    // 入力フォームに間違った情報を入力して送信
+    driver.findElement(By.name('username')).sendKeys('foo');
+    driver.findElement(By.name('password')).sendKeys('bar');
+    driver.findElement(By.css('.loginForm')).submit();
+
+    // エラーメッセージが表示されていることを検証
+    driver.findElement(By.css('.message')).getText().then(function(text) {
+      expect(text).to.be('Invalid username or password.');
     });
   });
 });
