d97eb8cd4a1e778bc67a95eff77441c3233b11b0
Kazuhito Hokamura <k.hokamura@gmail.com>
2014-12-04 18:54:10 +0900

    iOSのテストをPage Object版に書き換え

--- tests/test/login_ios.js
+++ tests/test/login_ios.js
@@ -1,7 +1,8 @@
 var webdriver = require('selenium-webdriver');
-var t = require('selenium-webdriver/testing');
-var By = webdriver.By;
 var expect = require('expect.js');
+var t = require('selenium-webdriver/testing');
+var TodoPage = require('./pages/todo');
+var LoginPage = require('./pages/login');
 var driver;
 
 t.describe('ログインページ', function() {
@@ -23,40 +24,30 @@ t.describe('ログインページ', function() {
   });
 
   t.it('トップにアクセスすると/loginにリダイレクトする', function() {
-    // トップページにアクセスする
-    driver.get('http://localhost:4000/');
+    var todoPage = new TodoPage(driver);
+    todoPage.open();
 
-    // 現在のURLを取得する
     driver.getCurrentUrl().then(function(url) {
-      // ログインページにリダイレクトされているかを検証する
-      expect(url).to.be('http://localhost:4000/login');
+      expect(url).to.be(LoginPage.URL);
     });
   });
 
   t.it('username/passwordが間違っていた場合にエラーが表示される', function() {
-    // ログインページにアクセス
-    driver.get('http://localhost:4000/login');
+    var loginPage = new LoginPage(driver);
 
-    // 入力フォームに間違った情報を入力して送信
-    driver.findElement(By.name('username')).sendKeys('foo');
-    driver.findElement(By.name('password')).sendKeys('bar');
-    driver.findElement(By.css('.loginForm')).submit();
-
-    // エラーメッセージが表示されていることを検証
-    driver.findElement(By.css('.message')).getText().then(function(text) {
-      expect(text).to.be('Invalid username or password.');
-    });
+    loginPage
+      .open()
+      .loginWithInvalidUser('foo', 'bar')
+      .getErrorMessage().then(function(text) {
+        expect(text).to.be('Invalid username or password.');
+      });
   });
 
   t.it('username/passwordが正しい場合はトップに遷移する', function() {
-    driver.get('http://localhost:4000/login');
+    var loginPage = new LoginPage(driver);
 
-    // 入力フォームに正しい情報を入力して送信
-    driver.findElement(By.name('username')).sendKeys('user');
-    driver.findElement(By.name('password')).sendKeys('pass');
-    driver.findElement(By.css('.loginForm')).submit();
+    loginPage.login('user', 'pass');
 
-    // トップページに遷移したかどうかを検証
     driver.wait(function() {
       return driver.getCurrentUrl().then(function(url) {
         return url === 'http://localhost:4000/';
